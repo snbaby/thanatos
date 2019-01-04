90e9963182156226e43380a61f6896d852cbd8e9
sn_baby <sn_baby@qq.com>
2019-01-04 11:57:00 +0800

    初始化

---
+++ src/main/java/com/seadun/thanatos/ThanatosApplication.java
@@ -0,0 +1,14 @@
+package com.seadun.thanatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanatosApplication.class, args);
+	}
+
+}
+
